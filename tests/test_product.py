a7d774d8d7bbba1d9a17fe62e482ea09ed4de257
Mikhail Lashukov <lashukovms@gmail.com>
2024-10-11 01:40:21 +0300

    add new classes and tests

---
+++ tests/test_product.py
@@ -0,0 +1,19 @@
+def test_init_product_1(product_1):
+    assert product_1.name == "Samsung Galaxy S23 Ultra"
+    assert product_1.description == "256GB, Серый цвет, 200MP камера"
+    assert product_1.price == 180000.0
+    assert product_1.quantity == 5
+
+
+def test_init_product_2(product_2):
+    assert product_2.name == "Iphone 15"
+    assert product_2.description == "512GB, Gray space"
+    assert product_2.price == 210000.0
+    assert product_2.quantity == 8
+
+
+def test_init_product_3(product_3):
+    assert product_3.name == "Xiaomi Redmi Note 11"
+    assert product_3.description == "1024GB, Синий"
+    assert product_3.price == 31000.0
+    assert product_3.quantity == 14
