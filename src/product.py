d6659317488ffc965fbf4bc8f8cfb975d171b5c0
Mikhail Lashukov <lashukovms@gmail.com>
2024-10-14 19:08:49 +0300

    fix classes category and product with new getter and setter

--- src/product.py
+++ src/product.py
@@ -3,5 +3,24 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, dict_product):
+        return cls(dict_product['name'], dict_product['description'], dict_product['price'], dict_product['quantity'])
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: int):
+        if new_price <= 0:
+            print('Цена не должна быть нулевая или отрицательная')
+        if new_price < self.__price:
+            answer = input('Введите "y", если согласны понизить цену, если не согласны введите "n"\n')
+            if answer == 'y':
+                self.__price = new_price
+
+
