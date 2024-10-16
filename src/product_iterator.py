978bb46aa1f3514b293bd096e382a63087082483
Mikhail Lashukov <lashukovms@gmail.com>
2024-10-16 16:34:10 +0300

    fix class product and category add new tests

---
+++ src/product_iterator.py
@@ -0,0 +1,14 @@
+class ProductIterator:
+    def __init__(self, category_obj):
+        self.category = category_obj
+        self.index = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.category.list_product):
+            self.index += 1
+            return self.category.list_product[self.index]
+        else:
+            return StopIteration
